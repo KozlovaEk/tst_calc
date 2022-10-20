3f9b54af40beb1cae43658ce33132dcea586c6df
Kozlova_Ekaterina <eakozlova@alfabank.ru>
2022-10-20 23:26:05 +0300

    tst2

--- calc.java
+++ calc.java
@@ -13,50 +13,53 @@ class calc{
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
-        String a = sc.next();
-        String op = sc.next();
-        String b = sc.next();
 
-        FileOutputStream fos=new FileOutputStream("output.txt");
+        FileOutputStream fos = new FileOutputStream("output.txt");
         PrintStream printStream = new PrintStream(fos);
 
-        try {
-            char[] aArr = a.toCharArray();
-            char[] bArr = b.toCharArray();
-            for (char c : aArr) {
-                // System.out.println(i+" "+String.valueOf(aArr[i]).equals("."));
-                if (!Character.isDigit(c) && !(String.valueOf(c).equals(".") || String.valueOf(c).equals("-"))) {
-                    throw new Exception("Error! Not number");
-                }
-            }
-            for (char c : bArr) {
-                if (!Character.isDigit(c) && !(String.valueOf(c).equals(".") || String.valueOf(c).equals("-"))) {
-                    throw new Exception("Error! Not number");
-                }
-            }
-            if (!op.equals("+") && !op.equals("-") && !op.equals("*") && !op.equals("/")) {
-                throw new Exception("Operation Error!");
-            }
-            if (op.equals("/") && b.equals("0")) {
-                throw new Exception("Error! Division by zero");
-            }
-            double a1 = Double.parseDouble(a);
-            double b1 = Double.parseDouble(b);
-            if (op.equals("+")) {
-                printStream.println(a1 + b1);
-            }
-            if (op.equals("-")) {
-                printStream.println(a1 - b1);
-            }
-            if (op.equals("*")) {
-                printStream.println(a1 * b1);
-            }
-            if (op.equals("/")) {
-                printStream.println(a1 / b1);
-            }
+        while (sc.hasNextLine()) {
+            String a = sc.next();
+            String op = sc.next();
+            String b = sc.next();
 
-        } catch (Exception ex) {
-            printStream.println(ex.getMessage());
+            try {
+                char[] aArr = a.toCharArray();
+                char[] bArr = b.toCharArray();
+                for (char c : aArr) {
+                    // System.out.println(i+" "+String.valueOf(aArr[i]).equals("."));
+                    if (!Character.isDigit(c) && !(String.valueOf(c).equals(".") || String.valueOf(c).equals("-"))) {
+                        throw new Exception("Error! Not number");
+                    }
+                }
+                for (char c : bArr) {
+                    if (!Character.isDigit(c) && !(String.valueOf(c).equals(".") || String.valueOf(c).equals("-"))) {
+                        throw new Exception("Error! Not number");
+                    }
+                }
+                if (!op.equals("+") && !op.equals("-") && !op.equals("*") && !op.equals("/")) {
+                    throw new Exception("Operation Error!");
+                }
+                if (op.equals("/") && b.equals("0")) {
+                    throw new Exception("Error! Division by zero");
+                }
+                double a1 = Double.parseDouble(a);
+                double b1 = Double.parseDouble(b);
+                if (op.equals("+")) {
+                    printStream.println(a + " " + op + " " + b + " = " + (a1 + b1));
+                }
+                if (op.equals("-")) {
+                    printStream.println(a + " " + op + " " + b + " = " + (a1 - b1));
+                }
+                if (op.equals("*")) {
+                    printStream.println(a + " " + op + " " + b + " = " + (a1 * b1));
+                }
+                if (op.equals("/")) {
+                    printStream.println(a + " " + op + " " + b + " = " + (a1 / b1));
+                }
+
+            } catch (Exception ex) {
+                printStream.println(a + " " + op + " " + b + " = " + ex.getMessage());
+            }
         }
     }
 }
